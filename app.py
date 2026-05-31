d2cd7787c59fd0f456684a47c4cde4d29ed8a162
haziel012 <hazielmoyacruz@gmail.com>
2026-05-30 21:30:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -433,4 +433,6 @@ def buscar_producto():
     return jsonify({"success": False, "mensaje": "Producto no encontrado"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
